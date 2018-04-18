e3f0f986a761c5277ec9e753cefc2b8231ded87e
Matabufalez <sixgunlovers@hotmail.com>
2018-04-18 13:04:58 +0200

    Initial commit

---
+++ src/servicios/Proceso.java
@@ -0,0 +1,38 @@
+package servicios;
+
+public class Proceso extends Thread {
+	
+	//Constructor
+	public Proceso(){
+		
+	}
+	
+	//Métodos para el incremento del tiempo lógico (Lamport)
+	public void LC1(){
+		
+	}
+	
+	public void LC2(){
+		
+	}
+	//Métodos para enviar los diferentes tipos de mensajes
+	
+	
+	//Métodos para recibir los diferentes tipos de mensajes
+	
+	
+	//Métodos para el envio de mensajes (multicast, unicast)
+	public void multicast(){
+		
+	}
+	public void unicast(){
+		
+	}
+	
+	//Método run
+	public void run(){
+	
+	}
+
+
+}
---
+++ src/servicios/Servidor.java
@@ -0,0 +1,42 @@
+package servicios;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Singleton 
+@Path("Servidor")
+public class Servidor {
+
+	
+	public Servidor(){
+		
+	}
+	
+	
+	@Path("start")
+	@GET 
+	@Produces(MediaType.TEXT_PLAIN)
+	public String start()
+	{
+		//Servicio para arrancar los procesos.
+		return "Process created sucessfully";
+	}
+	
+	@GET //tipo de petición HTTP
+	@Produces(MediaType.TEXT_PLAIN) //tipo de texto devuelto
+	@Path("enviarMensaje") //ruta al método
+	public String enviarMensaje() //el método debe retornar String
+	{ 
+		//
+		return "MENSAJE ENVIADO";
+	}
+	
+
+	
+	//
+	
+	
+}
