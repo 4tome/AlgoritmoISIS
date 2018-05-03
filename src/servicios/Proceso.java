8161b90d77bf98a263b2420b9a5c3cc230e657fa
AMateos <albertorres03@gmail.com>
2018-05-03 17:00:55 +0200

    Cambios en los metodos run y multicast
    Cambio de estos para que hagas lo que toca

--- src/servicios/Proceso.java
+++ src/servicios/Proceso.java
@@ -16,7 +16,6 @@ public class Proceso extends Thread {
 	private int contador;
 	private List<Mensaje> cola = new ArrayList<Mensaje>();
 	//private int Ci, Cj;
-	//usare orden de momento, porque con Ci y Cj no me aclaro.
 	private int orden;
 	private Semaphore semTiempo;
 	private Mensaje mensaje;//mensaje que envia el proceso
@@ -27,28 +26,25 @@ public class Proceso extends Thread {
 		this.id = id;
 		this.orden = time;
 		this.semTiempo = new Semaphore(1);
-		this.numProcess = numProcess - 1;
+		this.numProcess = numProcess;
 	}
 	
 	//Metodos para el incremento del tiempo logico (Lamport)
-	public void LC1(int tLamport){
+	public void LC1(){
 		try {
 		      semTiempo.acquire(1);
-		      tLamport += 1;
+		      this.orden += 1;
 		      semTiempo.release(1);
 		    } catch (InterruptedException e) {
 		      // TODO Auto-generated catch block
 		      e.printStackTrace();
 		    }
-		
-		//Creo que no haria falta pasar la variable reloj al proceso
-		//no entiendo lo de los tiempos
 	}
 	
-	public void LC2(int tLamport, int tLamportNew){
+	public void LC2(int tLamportNew){
 	    try {
 	      semTiempo.acquire(1);
-	      tLamport = Math.max(tLamport, tLamportNew) + 1;
+	      this.orden = Math.max(this.orden, tLamportNew) + 1;
 	      semTiempo.release(1);
 	    } catch (InterruptedException e) {
 	      // TODO Auto-generated catch block
@@ -57,25 +53,22 @@ public class Proceso extends Thread {
 	    
 		
 	}
+	
 	//Metodos para enviar los diferentes tipos de mensajes
 	public String mensaje(int orden)
 	{
-		//Creamos el Mensaje
 		String newId = "P" + this.id + " " + this.contador;
-		//Creamos el String que se enviará.
 		String Mensaje = newId + ";" + this.orden + ";" + " " + ";";
 		mensaje = new Mensaje(newId, this.orden, "", 0);
 		return Mensaje;
 	}
 	
 	public String propuesta (String id, int orden) {
-		//Creacion del String
 		String Propuesta = id + ";" + orden + ";" + "PROVISIONAL" + ";";
 		return Propuesta;
 	}
 	
 	public String acuerdo(String id, int orden) {
-		//Creacion del String
 		String Propuesta = id + ";" + orden + ";" + "DEFINITIVO" + ";";
 		return Propuesta;
 	}
@@ -83,23 +76,19 @@ public class Proceso extends Thread {
 	//Metodos para recibir los diferentes tipos de mensajes
 	public void recibirMsg(String msg)
 	{
-		//Descomposición del mensaje
 		String[] parts = msg.split(";");
-		String tipo = "PROVISIONAL";
-		//Comprobar tipo de mensaje
-		if(parts[2] != null && parts[2].equals(tipo)) {
+		if(parts[2] != null && parts[2].equals("PROVISIONAL")) {
 			//Al recibir un mensaje de propuesta
 			mensaje.setOrden(Math.max(mensaje.getOrden(), Integer.parseInt(parts[1])));
-			LC2(this.orden, Integer.parseInt(parts[1]));
+			LC2(Integer.parseInt(parts[1]));
 			mensaje.setNumP(mensaje.getNumP() + 1);
 			if (mensaje.getNumP() == this.numProcess) {
-				//Enviar mensaje de acuerdo.
 				mensaje.setState("DEFINITIVO");
 				this.unicast(acuerdo(mensaje.getId(),  mensaje.getOrden()), 0);
 			}
 			
 		}else{
-			LC1(this.orden);
+			LC1();
 			cola.add(new Mensaje(parts[0], Integer.parseInt(parts[1]), "PROVISIONAL", 0));
 			//System.out.println("mensaje recibido");
 			//Enviar propuesta
@@ -110,19 +99,12 @@ public class Proceso extends Thread {
 	
 	
 	//Metodos para el envio de mensajes (multicast, unicast)
-	public void multicast(){
-		//Creamos el mensaje
-		for(contador =0; contador<1; contador++) {
-			this.unicast(mensaje(this.orden), 0);
-			//Tiempo de espera
-			try {
-				long time = (long)(Math.random()*(5-2)+2);
-				Thread.sleep(time*100);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+	public void multicast(String mensaje){
+		Client proceso = ClientBuilder.newClient();
+		URI uri = UriBuilder.fromUri("http://localhost:8080/AlgoritmoISIS").build();
+		WebTarget target = proceso.target(uri);
+		//Llamar al servicio
+		System.out.println(target.path("rest/Servidor/enviarMensaje").queryParam("mensaje", mensaje).queryParam("destino", 0).request(MediaType.TEXT_PLAIN).get(String.class));
 		
 	}
 	
@@ -137,8 +119,17 @@ public class Proceso extends Thread {
 	
 	//Metodo run
 	public void run(){
-		//Mandar 1 mensaje normal
-		this.multicast();
+		//bucle de envio de mensajes
+		for(contador =0; contador<1; contador++) {
+			try {
+				this.multicast(mensaje(this.orden));
+				long time = (long)(Math.random()*(5-2)+2);
+				Thread.sleep(time*100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 
